d7818ecd8726c0f88a8e646dbab8ab6ec5a6887b
ray03j <eihouse.potato@outlook.jp>
2024-09-01 18:41:10 +0900

    [add] first api

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
-from routers import game
+from routers import player, team, game
+from db.session import engine
+from db.models import Base
 
 app = FastAPI()
 
-app.include_router(game.router)
+Base.metadata.create_all(bind=engine)
+
+app.include_router(player.router, prefix="/players", tags=["players"])
+app.include_router(team.router, prefix="/teams", tags=["teams"])
+app.include_router(game.router, prefix="/game", tags=["game"])
