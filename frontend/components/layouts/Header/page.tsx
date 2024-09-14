651333b676b0a30858c3805c652c14707966db2a
Yusuje <you030618.jp@gmail.com>
2024-09-14 18:22:41 +0900

    [add] userinfo

--- frontend/components/layouts/Header/page.tsx
+++ frontend/components/layouts/Header/page.tsx
@@ -7,10 +7,10 @@ export const Header = () => {
       <div className={styles.logo}>栄冠イレブン</div>
       <nav className={styles.nav}>
         <ul>
-          <li><Link href="/">ホーム</Link></li>
           <li><Link href="/players">選手一覧</Link></li>
           <li><Link href="/matches">戦術設定</Link></li>
           <li><Link href="/results">試合</Link></li>
+          <li><Link href="/userinfo">ユーザー設定</Link></li>
         </ul>
       </nav>
     </header>
