deebf536518037abf13bcab457e89f92063cc311
ray03j <eihouse.potato@outlook.jp>
2024-09-16 21:57:14 +0900

    [fix] get user by email or id

--- backend/api/cruds/user.py
+++ backend/api/cruds/user.py
@@ -1,14 +1,18 @@
 from passlib.context import CryptContext
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Session
-from db.models import User  
+from db.models import User
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_password_hash(password):
     return pwd_context.hash(password)
 
-def get_user(db: Session, email: str):
+def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email == email).first()
+
+def get_user_by_id(db: Session, user_id: UUID):
+    return db.query(User).filter(User.user_id == user_id).first()
 
 def create_user(db: Session, name: str, email: str, hashed_password: str):
     db_user = User(name=name, email=email, hashed_password=hashed_password)
