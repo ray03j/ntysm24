6d8f3ef8a09d88825c1d8a1248cc0bc1f0cbc35c
ray03j <eihouse.potato@outlook.jp>
2024-09-18 13:22:48 +0900

    [add] CORS

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from routers import player, team, match, user, practice
 from db.session import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # ReactアプリのURL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
